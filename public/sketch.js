b3cc849c4c702f0d7cd42d679c12b6260a358025
Dopaman <niklas.schmidt@hotmail.de>
2020-01-29 11:50:53 +0100

    update

--- public/sketch.js
+++ public/sketch.js
@@ -1,66 +1,109 @@
-/* © 2009 ROBO Design
- * http://www.robodesign.ro
- */
-var canvas = document.getElementById("drawCanvas");
-var ctx = canvas.getContext("2d");
-ctx.fillStyle = "#FF0000";
-ctx.fillRect(0, 0, 150, 75);
+
 // Keep everything in anonymous function, called on window load.
 if(window.addEventListener) {
-window.addEventListener('load', function () {
-  var canvas, context;
+	window.addEventListener('load', function () {
+		var canvas, context, tool, socket;
+
+		socket = io.connect('http://localhost:3000');
 
   // Initialization sequence.
   function init () {
     // Find the canvas element.
     canvas = document.getElementById('drawCanvas');
     if (!canvas) {
-      alert('Error: I cannot find the canvas element!');
-      return;
+    	alert('Error: I cannot find the canvas element!');
+    	return;
     }
 
     if (!canvas.getContext) {
-      alert('Error: no canvas.getContext!');
-      return;
+    	alert('Error: no canvas.getContext!');
+    	return;
     }
 
     // Get the 2D canvas context.
     context = canvas.getContext('2d');
     if (!context) {
-      alert('Error: failed to getContext!');
-      return;
+    	alert('Error: failed to getContext!');
+    	return;
     }
-    // Attach the mousemove event handler.
-    canvas.addEventListener('mousemove', ev_mousemove, false);
-  }
 
-  // The mousemove event handler.
-  var started = false;
-  function ev_mousemove (ev) {
-    var x, y;
+    //Creates an instance of the tool pencil.
+    tool = new tool_pencil();
+
+    // Attach the mousedown, mousemove and mouseup event handler.
+    canvas.addEventListener('mousedown', ev_canvas, false);
+    canvas.addEventListener('mousemove', ev_canvas, false);
+    canvas.addEventListener('mouseup', ev_canvas, false);
+}
+socket.on('mouse', newMousemove);
+
+function newMousemove(data) {
+
+	context.lineTo(data.x, data.y);
+	context.stroke();
+}
+
+function tool_pencil() {
+	var tool = this;
+	this.started = false;
+
+
+//This is called when you hold down your mouse button.
+//It starts the pencil drawing.
+	this.mousedown = function (ev) {
+		context.beginPath();
+		context.moveTo(ev._x, ev._y);
+		tool.started = true;
+	};
+
+	//This function is called every time you move your mouse.
+	//It only draws, when the tool is started(when holding down the mouse button).
+	this.mousemove = function (ev) {
+
+		if (tool.started) {
+
+			//data ist ein Object, x und y sind keys/properties.
+			var data = {x:ev._x, y:ev._y};
+			context.lineTo(ev._x, ev._y);
+			context.stroke();
+			console.log(ev._x + ',' + ev._y);
+			socket.emit('mouse', data);
+		}
+	};
+
+	this.mouseup = function (ev) {
+		if (tool.started) {
+			tool.mousemove(ev);
+			tool.started = false;
+		}
+	};
+}
+
+
+  function ev_canvas (ev) {
 
     // Get the mouse position relative to the canvas element.
     if (ev.layerX || ev.layerX == 0) { // Firefox
-      x = ev.layerX;
-      y = ev.layerY;
+    	ev._x = ev.layerX;
+    	ev._y = ev.layerY;
     } else if (ev.offsetX || ev.offsetX == 0) { // Opera
-      x = ev.offsetX;
-      y = ev.offsetY;
+    	ev._x = ev.offsetX;
+    	ev._y = ev.offsetY;
     }
 
     // The event handler works like a drawing pencil which tracks the mouse 
     // movements. We start drawing a path made up of lines.
-    if (!started) {
-      context.beginPath();
-      context.moveTo(x, y);
-      started = true;
-    } else {
-      context.lineTo(x, y);
-      context.stroke();
-    }
-  }
+  	var func = tool[ev.type];
+  	if (func) {
+  		func(ev);
+  	}
+}
 
-  init();
+init();
 }, false); }
 
 // vim:set spell spl=en fo=wan1croql tw=80 ts=2 sw=2 sts=2 sta et ai cin fenc=utf-8 ff=unix:
+
+
+
+
--- server.js
+++ server.js
@@ -9,4 +9,21 @@ var server = app.listen(3000);
 //Erlaubt im Ordner public alle statischen Dateien zu hosten,
 //sprich es werden dem Benutzer die Dateien aus dem Ordner angezeigt.
 app.use(express.static('public'));
+
 console.log("My socket server is running!");
+
+var socket = require('socket.io');
+
+var io = socket(server);
+
+//Betrachtet die Ankunft von Nachrichten auf allen Sockets.
+io.sockets.on('connection', newConnection);
+
+function newConnection(socket) {
+	console.log('new connection: ' + socket.id);
+	socket.on('mouse', mouseMsg);
+	function mouseMsg(data) {
+		socket.broadcast.emit('mouse', data);
+		console.log(data);
+	}
+}
